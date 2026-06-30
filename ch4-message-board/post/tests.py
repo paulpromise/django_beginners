44377bda12302de875dec2db328c2c6ad51d70f6
Paul Promise <paul.promise@outlook.com>
2026-06-30 17:47:49 +0000

    initial commit

--- ch4-message-board/post/tests.py
+++ ch4-message-board/post/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Post
+from django.urls import reverse
 
 # Create your tests here.
+class test_homepage(TestCase):
+
+    def TestHomePage(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
